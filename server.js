f56489b93b2e58e4099fb7f684c74503b7b47096
liu-matt <matt_liu@apple.com>
2021-04-17 13:13:47 -0400

    a

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 // TODO: visit https://expressjs.com/
 const express = require('express')
 const app = express()
-
+var cors = require('cors');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://admin:liuxiao123@cluster0.nqnao.mongodb.net/data?retryWrites=true&w=majority',
     {useNewUrlParser: true, useUnifiedTopology: true});
 
 // Configures CORS
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers',
-        'Content-Type, X-Requested-With, Origin');
-    res.header('Access-Control-Allow-Methods',
-        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    next();
-});
+app.use(cors());
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))

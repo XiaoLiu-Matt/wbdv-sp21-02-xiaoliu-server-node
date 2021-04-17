718ff4485644bb248968d49e7989ec2c9f41bd64
liu-matt <matt_liu@apple.com>
2021-04-17 14:08:36 -0400

    c

--- server.js
+++ server.js
@@ -7,7 +7,15 @@ mongoose.connect('mongodb+srv://admin:liuxiao123@cluster0.nqnao.mongodb.net/data
     {useNewUrlParser: true, useUnifiedTopology: true});
 
 // Configures CORS
-app.use(cors());
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Headers',
+        'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
